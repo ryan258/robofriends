207f0559651339ca8080a47dcb4a3fb7db4965d7
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-10 22:14:16 -0500

    Pull data from a mock API

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react'
 import CardList from './components/CardList'
 import SearchBox from './components/SearchBox'
 
-import { robots } from './data/robots'
+// import { robots } from './data/robots'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+    // console.log('constructor triggers')
 
     // this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  componentDidMount() {
+    // console.log('component mounted!!')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }))
   }
 
   onSearchChange = (event) => {
@@ -21,17 +29,22 @@ class App extends Component {
   }
 
   render() {
+    console.log('boom! render!')
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     })
 
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      )
+    }
   }
 }
 
