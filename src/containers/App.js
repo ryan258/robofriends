b4ef966a7b3298f2c530588590a2a75e9c4a2f5a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-12 09:20:05 -0500

    Refactor file structure and a Scroll component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import CardList from './components/CardList'
-import SearchBox from './components/SearchBox'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
 
 // import { robots } from './data/robots'
 import './App.css'
@@ -19,6 +20,7 @@ class App extends Component {
 
   componentDidMount() {
     // console.log('component mounted!!')
+    // since fetch isn't attached to anything, it's actually become part of the window object (window.fetch)
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }))
@@ -29,22 +31,23 @@ class App extends Component {
   }
 
   render() {
-    console.log('boom! render!')
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    // console.log('boom! render!')
+    const { robots, searchField } = this.state
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
           <CardList robots={filteredRobots} />
-        </div>
-      )
-    }
+        </Scroll>
+      </div>
+    )
   }
 }
 
