2bbc5661ccd0138b9f7c976575df3adb63275336
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-15 09:15:28 -0500

    Clean out comments in prep for Redux implementation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,8 @@
-// useState - basically state handling
-// useEffect - lifecycle component esq - componentDidMount ~= [], componentDidUpdate, componentWillUnmount
 import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 
-// import { robots } from './data/robots'
 import './App.css'
 import ErrorBoundary from '../components/ErrorBoundary'
 
@@ -18,17 +15,12 @@ const App = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => setRobots(users))
-    console.log('a fetch happened')
   }, [])
-  useEffect(() => {
-    console.log(count)
-  }, [count])
 
   const onSearchChange = (event) => {
     setSearchField(event.target.value)
   }
 
-  // const { robots, searchField } = state
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase())
   })
@@ -51,52 +43,4 @@ const App = () => {
   )
 }
 
-/*
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
-    // console.log('constructor triggers')
-
-    // this.onSearchChange = this.onSearchChange.bind(this)
-  }
-
-  componentDidMount() {
-    // console.log('component mounted!!')
-    // since fetch isn't attached to anything, it's actually become part of the window object (window.fetch)
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }))
-  }
-
-  onSearchChange = (event) => {
-    this.setState((prevState) => ({ searchField: event.target.value }))
-  }
-
-  render() {
-    // console.log('boom! render!')
-    const { robots, searchField } = this.state
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    })
-
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    )
-  }
-}
-*/
 export default App
