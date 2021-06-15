5faf422b4864a5705a1685066c3e0bac401d042a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-15 08:35:00 -0500

    Turn <App> into a functional component w/ hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+// useState - basically state handling
+// useEffect - lifecycle component esq - componentDidMount ~= [], componentDidUpdate, componentWillUnmount
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
@@ -7,6 +9,49 @@ import Scroll from '../components/Scroll'
 import './App.css'
 import ErrorBoundary from '../components/ErrorBoundary'
 
+const App = () => {
+  const [robots, setRobots] = useState([])
+  const [searchField, setSearchField] = useState('')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setRobots(users))
+    console.log('a fetch happened')
+  }, [])
+  useEffect(() => {
+    console.log(count)
+  }, [count])
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value)
+  }
+
+  // const { robots, searchField } = state
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  console.log(robots, searchField)
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  )
+}
+
+/*
 class App extends Component {
   constructor() {
     super()
@@ -53,5 +98,5 @@ class App extends Component {
     )
   }
 }
-
+*/
 export default App
